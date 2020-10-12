4e85e4c4a1e65ef2d81fb442a9bf74577e8af62d
Tiago Henrique <tihemetene@gmail.com>
2020-10-12 20:12:55 -0300

    Update EquacaoExplicita.java

--- src/algorithms/EquacaoExplicita.java
+++ src/algorithms/EquacaoExplicita.java
@@ -10,8 +10,22 @@ public class EquacaoExplicita extends LineAlgorithm{
 	}
 
 	@Override
-	public void draw(int x1, int y1, int x2, int y2) {
-		// TODO Implementar o método da equação explícita aqui
+	public void draw(GL2 gl, int x1,int x2, int y1,int y2) {
+		int y_aux;
+		
+		int x;
+		float m, y;
+		int dx, dy;
+		dx = Math.abs(x2 - x1);
+		dy = Math.abs(y2 - y1);
+		m = (float)dy/dx;
+		x = x1;
+		while(x <= x2) {
+			y = m * (x - x1) + y1;
+			y_aux = Math.round(y);
+			draw_pixel(gl, x, y_aux, "GREEN");
+			x++;
+		}
 	}
 
 }
